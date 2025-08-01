2bd62a61be6bc9abe8b1b0aa405394c6c52318d9
c0smic-c0mput3r <mothersiyaa@gmail.com>
2025-08-01 22:50:29 +0530

    brief intro and greet in python

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+print("hello, world\n")
+name="c0smic c4t"
+print(f"im the {name}, an uber nerd\n") #formatted string input 
+print("execution successful :3")
+# in python the default end line character 4 the print() is a newline character "\n"
+
